3c74b74e9b9a138a31f0cb9fac3ed69d7e8ff992
camila <camila@LAPTOP-N0GHACTF>
2022-07-07 12:14:12 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio3NumerosPrimos.Consola/Program.cs
@@ -0,0 +1,28 @@
+﻿using EjercicioNumerosprimos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3NumerosPrimos.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primo = new Primo();
+            if (primo.Validar())
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Objeto no válido");
+            }
+
+            Console.WriteLine($"");
+
+        }
+    }
+}
---
+++ EjercicioNumerosprimos.Entidades/Primo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNumerosprimos.Entidades
+{
+    public class Primo
+    {
+        //El atributo n es de tipo entero y privado
+        private int n { get; set; }
+        //Debe tener un método para informar dicho atributo.
+       public int GetNUmero()
+       {
+            return n;
+       }
+
+        //  Otro atributo privado de tipo array de enteros donde se almacenarán los
+        //términos generados
+        private int[] numerosPrimos;
+
+
+
+        //Definir 2 constructores, uno que tome un parámetro que inicializará el
+        //atributo n y otro que no reciba parámetros que por defecto inicializará el
+        //atributo n en 5.
+
+        public Primo(int N)
+        {
+            n = N;
+
+        }
+
+        public Primo():this(5)
+        {
+            
+        }
+
+
+
+        //Definir un método para validar el objeto Primos.
+
+        public bool Validar()
+        {
+         return   n>= 1;
+        }
+
+
+        //Definir un método que genere los términos de la serie.
+        public void GenerarPrimos()
+        {
+            numerosPrimos = new int[n];
+
+            int contadorPosiciones = 0;
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                if (VerificarNroPrimo(i))
+                {
+                    numerosPrimos[contadorPosiciones] = i;
+                    contadorPosiciones++;
+                    if (contadorPosiciones==n)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private bool VerificarNroPrimo(int i)
+        {
+            //Poner el algoritmo para ver si es primo el nro pasado
+            return true;
+        }
+
+        //Definir un método público que me permita acceder a un término de la serie, si
+        // el término no existiere arrojar una excepción.
+
+       
+
+
+
+
+    }
+}
